6139d5639a48678a6608ef3d17c0f876ebfb6972
臧戎 <zangrong@zangrongdemacbook-pro.local>
2016-11-09 21:33:58 +0800

    Initial Commit

---
+++ tab1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tab1
+//
+//  Created by 臧戎 on 16/11/9.
+//  Copyright © 2016年 cetian. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
